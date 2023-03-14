f3f3ac793a30c8707138fffa7780f822f49c873a
kenny <engr.omokehinde@gmail.com>
2023-03-14 23:21:19 +0000

    store application in database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,13 @@ def show_job(id):
   if not JOB:
     return 'Note found', 494
   return render_template('jobpage.html', job=JOB)
+
+@app.route('/job/<id>/apply', methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  add_application_to_db(id, data)
+  return render_template('applicationSubmitted.html', 
+                        application=data)
 
 @app.route("/api/jobs")
 def list_jobs():
--- database.py
+++ database.py
@@ -30,4 +30,14 @@ def load_job_from_db(id):
       return None
     result = dict(result[0]._mapping)
   return result
-# print(load_job_from_db(2))
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text('INSERT INTO applications(job_id, full_name, email, linkedin_url, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, :resume_url)')
+    conn.execute(query, {
+      "job_id": job_id,
+      "full_name": data['full_name'],
+      "email": data['email'],
+      "linkedin_url": data['linkedin_url'],
+      "resume_url": data['resume_url'] 
+    })
